1ed5fa3b1d5c152901103b74a351c80fadc02438
Krushna <krush15699@gmail.com>
2024-05-04 11:37:20 +0530

    No Issue

--- src/main/java/com/Interceptor/InterceptorDemo/InterceptorDemoApplication.java
+++ src/main/java/com/Interceptor/InterceptorDemo/InterceptorDemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InterceptorDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(InterceptorDemoApplication.class, args);
+		System.out.println("Good to Go");
 	}
 
 }
